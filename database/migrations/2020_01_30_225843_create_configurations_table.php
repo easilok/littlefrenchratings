20e007c35da6bba9e028c31591ec206f15aea52f
Luis Pereira <luispereira.tkd@gmail.com>
2020-02-02 23:10:49 +0000

    Added default and value fields to configuration table. More specification text

--- database/migrations/2020_01_30_225843_create_configurations_table.php
+++ database/migrations/2020_01_30_225843_create_configurations_table.php
@@ -16,6 +16,7 @@ class CreateConfigurationsTable extends Migration
         Schema::create('configurations', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 100)->default("")->unique();
+            $table->string('default');
             $table->unsignedBigInteger('role_id');
 						$table->boolean('enable')->default(true);
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
@@ -25,6 +26,7 @@ class CreateConfigurationsTable extends Migration
         Schema::create('configuration_user', function (Blueprint $table) {
             $table->unsignedBigInteger('configuration_id');
             $table->unsignedBigInteger('user_id');
+            $table->string('value');
             $table->foreign('configuration_id')->references('id')->on('configurations')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->primary(['user_id', 'configuration_id']);
